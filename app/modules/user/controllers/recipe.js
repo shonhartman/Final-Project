14230d919c60660366a9fe7157ad5f3f270379e8
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-05-10 11:43:48 -0500

    adding timer

--- app/modules/user/controllers/recipe.js
+++ app/modules/user/controllers/recipe.js
@@ -1,7 +1,57 @@
 class RecipeController {
-  constructor($http) {
+  constructor($http, $scope, $interval) {
     this._$http = $http;
+    $scope.timeLeft = 300;
+
+//timer/////////////////////////////////////////////////
+    $scope.startTime = function() {
+      $scope.disabled = true;
+    }
+
+    time = $interval(function() {
+      $scope.timeLeft--;
+      if($scope.timeLeft <= 0) {
+        $scope.disabled = true;
+        $interval.cancel(time);
+        sound.play();
+      }
+    }, 1000);
+
+  $scope.stopTime = function() {
+    $scope.disabled = false;
+
+    $interval.cancel(time);
   }
+
+  $scope.resetTime = function() {
+    $scope.disabled = false;
+
+    $scope.timeLeft = 300;
+  }
+
+  timer.filter('timecode', function() {
+  return function(seconds) {
+    seconds = Number.parseFloat(seconds);
+
+    var wholeSeconds = Math.floor(seconds);
+    var minutes = Math.floor(wholeSeconds / 60);
+
+    remainingSeconds = wholeSeconds % 60;
+
+    output = minutes + ':';
+
+    if(remainingSeconds < 10) {
+      output += '0';
+    }
+
+    output += remainingSeconds;
+
+    return output;
+  }
+});
+//////////////////////////////////////////////////////////////
+
+}
 
   scrape() {
     console.log("scraping");
@@ -16,7 +66,6 @@ class RecipeController {
 
           let title = el.querySelector(".main-title");
           console.log(title.textContent);
-
       })
         .catch(error => {
           console.error(error);
@@ -24,5 +73,6 @@ class RecipeController {
 
   }
 }
+
 
 export default RecipeController;
